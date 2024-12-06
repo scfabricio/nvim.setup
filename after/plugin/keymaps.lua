967f7596c4798f2f2a82bed85a5e35000a4cf40e
Fabricio Carneiro <scfabricio15@gmail.com>
2024-12-06 10:01:34 -0300

    feat: comando git pull e push

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -14,8 +14,8 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git Status" }) -- Status do Git
 vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git Commit" }) -- Fazer commit
 vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", { desc = "Git Add All" }) -- Fazer add
-vim.keymap.set("n", "<leader>gps", ":Git push<CR>", { desc = "Git Push" }) -- Fazer push
-vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>", { desc = "Git Pull" }) -- Fazer pull
+vim.keymap.set("n", "<leader>gP", ":Git push<CR>", { desc = "Git Push" }) -- Fazer push
+vim.keymap.set("n", "<leader>gp", ":Git pull<CR>", { desc = "Git Pull" }) -- Fazer pull
 vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git Blame" }) -- Blame
 vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "Git Diff" }) -- Diff
 
